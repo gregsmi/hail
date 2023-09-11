f2e6d868c052ffe0620988493ad67730561380c6
Greg Smith <gregsmi@microsoft.com>
2023-09-11 12:02:45 -0700

    update for redeploy

--- infra/bootstrap_utils.sh
+++ infra/bootstrap_utils.sh
@@ -11,7 +11,6 @@ source $HAIL/devbin/functions.sh
 
 copy_images() {
     make -C $HAIL/docker/third-party copy
-
     make -C $HAIL/hail python/hail/hail_pip_version
     make -C $HAIL/docker/hailgenetics mirror-dockerhub-images
 }
